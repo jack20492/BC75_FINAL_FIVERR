983bc126507e8dd512c4298788874ae7a1e4d513
jack20492 <nphuc210@gmail.com>
2025-01-21 22:57:05 +0700

    Update get-comment-api.ts

--- src/apis/get-comment-api.ts
+++ src/apis/get-comment-api.ts
@@ -1,8 +1,8 @@
 import api from "./base";
 
-export const getCommentApi = async (maCongViec:number) => {
+export const getCommentApi = async (MaCongViec:number) => {
     try {
-      const response = await api.get(`/binh-luan/lay-binh-luan-theo-cong-viec/${maCongViec}`);
+      const response = await api.get(`/binh-luan/lay-binh-luan-theo-cong-viec/${MaCongViec}`);
       return response.data.content;
     } catch (error: any) {
       throw error.response;
