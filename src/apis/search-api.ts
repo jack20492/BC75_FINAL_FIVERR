d46faa0265b18e4f47fd56f79858ae9bd48dc494
jack20492 <nphuc210@gmail.com>
2025-01-21 23:18:40 +0700

    Update search-api.ts

--- src/apis/search-api.ts
+++ src/apis/search-api.ts
@@ -1,9 +1,9 @@
 import api from "./base";
 
-export const searchApi = async (TenCongViec: string) => {
+export const searchApi = async (tenCongViec: string) => {
   try {
     const response = await api.get(
-      `/cong-viec/lay-danh-sach-cong-viec-theo-ten/${TenCongViec}`
+      `/cong-viec/lay-danh-sach-cong-viec-theo-ten/${tenCongViec}`
     );
     return response.data.content;
   } catch (error: any) {
