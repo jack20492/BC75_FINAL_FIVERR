c21266b04270e7fcee55528df2b321a639fb7c45
jack20492 <nphuc210@gmail.com>
2025-01-21 22:55:34 +0700

    Update listJob-detail-api.ts

--- src/apis/listJob-detail-api.ts
+++ src/apis/listJob-detail-api.ts
@@ -1,8 +1,8 @@
 import api from "./base";
 
-export const listjobdetailApi = async (maChiTietLoai:number) => {
+export const listjobdetailApi = async (MaChiTietLoai:number) => {
     try {
-      const response = await api.get(`/cong-viec/lay-cong-viec-theo-chi-tiet-loai/${maChiTietLoai}`);
+      const response = await api.get(`/cong-viec/lay-cong-viec-theo-chi-tiet-loai/${MaChiTietLoai}`);
       return response.data.content;
     } catch (error: any) {
       throw error.response;
